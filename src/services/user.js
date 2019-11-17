b4124770090b4ce60bf10cfe73cb5371ebd11ad3
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2019-11-16 23:21:34 -0600

    Request lorem ipsum text
    Use Bacon Ipsum API

--- src/services/user.js
+++ src/services/user.js
@@ -2,10 +2,18 @@ export async function getUsers(size = 12, page = 1) {
   const params = {
     results: size,
     page,
+    nat: 'US',
   };
-  const response = await fetch(
+  const userResp = await fetch(
     `https://randomuser.me/api/?${new URLSearchParams(params).toString()}`,
   );
-  const json = await response.json();
-  return json.results;
+  const loremResp = await fetch(
+    `https://baconipsum.com/api/?type=all-meat&format=text&paras=${12}`,
+  );
+  const json = await userResp.json();
+  const paragraphs = (await loremResp.text()).split('\n\n');
+  return json.results.map((user, idx) => ({
+    ...user,
+    lorem: paragraphs[idx],
+  }));
 }
